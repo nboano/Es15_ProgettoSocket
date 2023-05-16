1853494f099d232847cd932c0cc43f05df1b3203
nboano <nboano@outlook.it>
2023-05-16 16:14:16 +0200

    Continuazione login

--- Es15_ProgettoSocket_Server/main.c
+++ Es15_ProgettoSocket_Server/main.c
@@ -12,17 +12,46 @@
 
 const HTTPResponse HTTP_NOT_FOUND = {
     404, "NOT FOUND", JSON_RESPONSE_HEADERS,
-    "{\"error\":{\"code\":404,\"message\":\"Risorsa non trovata.\"}}"
+    "{\"ok\":false,\"error\":{\"code\":404,\"message\":\"Risorsa non trovata.\"}}"
 };
 
 const HTTPResponse HTTP_METHOD_NOT_ALLOWED = {
     405, "METHOD NOT ALLOWED", JSON_RESPONSE_HEADERS,
-    "{\"error\":{\"code\":405,\"message\":\"Metodo HTTP non consentito.\"}}"
+    "{\"ok\":false,\"error\":{\"code\":405,\"message\":\"Metodo HTTP non consentito.\"}}"
 };
+
+const HTTPResponse HTTP_UNAUTHORIZED = {
+    401, "UNAUTHORIZED", JSON_RESPONSE_HEADERS,
+    "{\"ok\":false,\"error\":{\"code\":401,\"message\":\"Credenziali errate o mancanti.\"}}"
+};
+
+HTTPResponse HTTP_BUILD_OK_RESPONSE(const char* data) {
+    HTTPResponse OkResponse = {200, "OK", JSON_RESPONSE_HEADERS};
+    sprintf(OkResponse.Body, "{\"ok\":true,\"data\":%s}", data);
+    return OkResponse;
+}
 
 NEW_SOCKET(miosock, BUFFER_LEN);
 
+char** perform_login(const char* username, const char* password);
+
+char* generate_id(char *str, int size)
+{
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJK1234567890#!-+/=@";
+    if (size) {
+        --size;
+        for (int n = 0; n < size; n++) {
+            int key = Random.InRange(0, sizeof(charset));
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+    return str;
+}
+
 unsigned long ServingThread(void* param) {
+
+    srand(time(NULL));
 
     SOCKET socket_clone = miosock.ClientSocket;
 
@@ -35,12 +64,23 @@ unsigned long ServingThread(void* param) {
     if(strcmp(req.Path, "/") == 0) {
 
         if(strcmp(req.Method, "POST") == 0) {
-            HTTPResponse ok_resp = {
-                200, "OK",  JSON_RESPONSE_HEADERS
-            };
-            sprintf(ok_resp.Body, "{\"ok\":true,\"data\":{\"ip\":\"%s\"}}", (char*)param);
 
-            resp = ok_resp;
+            char* username = strtok(req.Body, ";");
+            char* password = strtok(NULL, ";");
+
+            char** login_row = perform_login(username, password);
+
+            if(login_row) {
+                char sid_bf[25] = ""; generate_id(sid_bf, 25);
+
+                char resp_bf[255] = "";
+                sprintf(resp_bf, "{\"session_id\":\"%s\",\"username\":\"%s\",\"ruolo\":%s,\"nome\":\"%s\",\"cognome\":\"%s\",\"status\":\"Login successful.\"}", sid_bf, login_row[0], login_row[1], login_row[2], login_row[3]);
+                
+                resp = HTTP_BUILD_OK_RESPONSE(resp_bf);
+            } else {
+                resp = HTTP_UNAUTHORIZED;
+            }
+
         } else {
             resp = HTTP_METHOD_NOT_ALLOWED;
         }
@@ -54,30 +94,22 @@ unsigned long ServingThread(void* param) {
     HTTP.SendResponseToSocket(socket_clone, resp);
 }
 
-void login(const char* username, const char* password) {
+char** perform_login(const char* username, const char* password) {
+
     Database.AddParameter("@username", username);
     Database.AddParameter("@password", password);
-    MYSQL_RES* result = Database.ExecuteQuery("SELECT username FROM autisti WHERE username = @username AND password = @password");
+    MYSQL_RES* result = Database.ExecuteQuery("SELECT username, ruolo, nome, cognome FROM autisti WHERE username = @username AND password = @password");
 
     MYSQL_ROW row = mysql_fetch_row(result);
-    if(row) {
-        printf("LOGIN OK: %s\n", username);
-    } else {
-        printf("LOGIN FALLITO: %s\n", username);
-    }
 
     mysql_free_result(result);
+
+    return row;
 }
 
 int main() 
 {
-    DB_Init();
-
-    login("consegne001", "consegne001");
-
-    login("consegne002", "consegne002");
-
-    login("consegne005", "consegne005");
+    Database.Connect();
 
     char message[50] = "";
     sprintf(message, "In attesa sulla porta %i.", PORT);
@@ -91,7 +123,7 @@ int main()
     miosock.Init();
     miosock.Listen(PORT);
     
-    while(1) {
+    while(true) {
         Sleep(10);
 
         char* connected_ip = miosock.WaitForConnection();
